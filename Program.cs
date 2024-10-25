d05ac1dfcd4040b0ca954591127c3ac04950f2a6
Niklas Bryngelsson <niklas.bryngelsson@brightstraining.com>
2024-10-25 14:30:38 +0200

    Updated info

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Ett sätt att slumpa ett tal mellan 1-100
+// Ge användaren info om vad den ska göra
+// Kunna läsa in användarens input 
+// Säga åt användaren att den hade fel
+// Säga till användare att den hade rätt
+// En game loop så att spelet inte avslutas efter första gissningen - while 
+// Max antal gissningar 
+// Räkna ut om spelaren har förslorat 
+// Ge spelaren tips om talet är högre eller lägre än gissningen
