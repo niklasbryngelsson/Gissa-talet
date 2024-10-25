3d21a1e45208c3018a5e631614f906eece1165a0
Niklas Bryngelsson <niklas.bryngelsson@brightstraining.com>
2024-10-25 16:14:12 +0200

    Uppdated: if-statements, life, numbers over 100

--- Program.cs
+++ Program.cs
@@ -5,19 +5,51 @@
 // Säga till användare att den hade rätt
 // En game loop så att spelet inte avslutas efter första gissningen - while 
 
-Random random= new Random();
-int randomValue = random.Next(1,101);
+Random random = new Random();
+int randomValue = random.Next(1, 101);
+
+int life = 7;
 
 while (true)
 {
-    Console.Write("Gissa ett tal mellan 1-100");
+    life--;
+    Console.WriteLine($"Antal liv: {life}");
+    if (life == 0)
+    {
+
+        TextWriter.FailText("Du förlorade");
+        break;
+    }
+    Console.Write("Gissa ett tal mellan 1-100: ");
     int input = 0;
 
-    if (int.TryParse(Console.ReadLine(), out input))
+    if (!int.TryParse(Console.ReadLine(), out input))
     {
-        
+        Console.WriteLine("Vänligen skriv ett tal");
+        continue;
     }
-    break; 
+
+    if (input > 100)
+    {
+        TextWriter.WarningText("Talet är över 100. Skriv in ett nytt tal");   
+    }
+
+    if (input > randomValue)
+    {
+        TextWriter.WarningText("Talet är lägre.");
+        continue;
+    }
+
+    if (input < randomValue)
+    {
+        TextWriter.WarningText("Talet är högre.");
+    }
+
+    if (input == randomValue)
+    {
+        TextWriter.SuccessText("Du gissade rätt!!");
+        break;
+    }
 }
 
 
@@ -25,3 +57,4 @@ while (true)
 // Max antal gissningar 
 // Räkna ut om spelaren har förlorat 
 // Ge spelaren tips om talet är högre eller lägre än gissningen
+// Lägg till kommentar om talet inte är inom  begränsingarna
