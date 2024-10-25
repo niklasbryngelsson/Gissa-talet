34a66a81fe1ceee44a4cd49e428d6678ffc6999a
Niklas Bryngelsson <niklas.bryngelsson@brightstraining.com>
2024-10-25 15:04:16 +0200

    Added while loop

--- Program.cs
+++ Program.cs
@@ -4,6 +4,22 @@
 // Säga åt användaren att den hade fel
 // Säga till användare att den hade rätt
 // En game loop så att spelet inte avslutas efter första gissningen - while 
+
+while (true)
+{
+    Console.Write("Gissa ett tal mellan 1-100");
+    int input = 0;
+
+    if (int.TryParse(Console.ReadLine(), out input))
+    {
+        
+    }
+    break; 
+}
+
+
+
+
 // Max antal gissningar 
-// Räkna ut om spelaren har förslorat 
+// Räkna ut om spelaren har förlorat 
 // Ge spelaren tips om talet är högre eller lägre än gissningen
